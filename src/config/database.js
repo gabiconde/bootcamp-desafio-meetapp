b43b06c09d85db8324d4134e161c480ddf375aa3
gabriela conde <gabiconde97@gmail.com>
2019-08-21 21:27:48 -0300

    database connection and configuration using sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+export default {
+  database: 'meetapp',
+  username: 'postgres',
+  password: 'docker',
+  dialect: 'postgres',
+  host: 'localhost',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
