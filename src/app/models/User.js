a218e649b708a54da3d957bbfb9fe50e3cbe93ae
gabriela conde <gabiconde97@gmail.com>
2019-08-27 17:08:04 -0300

    User Update and Data validation

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,26 +1,34 @@
-import { Model, Sequelize} from  'sequelize'
+import { Model, Sequelize } from 'sequelize';
 
 import bcrypt from 'bcrypt';
 
 class User extends Model {
-
-    static init(sequelize){
-      super.init({
+  static init(sequelize) {
+    super.init(
+      {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
         password: Sequelize.VIRTUAL,
         speaker: Sequelize.BOOLEAN,
-      },{
+      },
+      {
         sequelize,
-      });
+      }
+    );
 
-    }
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
 
-    checkPassword(password){
-      return password === this.password_hash
-      //bcrypt
-    }
+    return this;
+  }
+
+  async checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
 }
 
 export default User;
